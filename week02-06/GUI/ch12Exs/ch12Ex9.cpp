ea799458d93b2d4fab33d5957b8098532233184f
sahwel <mezosijoci@gmail.com>
2021-03-02 15:18:56 +0100

    edit comments

--- week02-06/GUI/ch12Exs/ch12Ex9.cpp
+++ week02-06/GUI/ch12Exs/ch12Ex9.cpp
@@ -1,5 +1,5 @@
 /*
-    g++ ch12Ex9.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o main `fltk-config --ldflags --use-images`
+    g++ ch12Ex9.cpp ../Graph.cpp ../Window.cpp ../GUI.cpp .//Simple_window.cpp  -o main `fltk-config --ldflags --use-images`
 */
 #include "../Simple_window.h"
 #include "../Graph.h"
